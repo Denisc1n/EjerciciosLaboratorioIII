eb3a961ba254a803e2c7aa089f5b7c45756f67d5
alumno <alumno>
2017-04-11 22:09:42 -0300

    script

---
+++ ClaseCuatro/scripts/scriptUno.js
@@ -0,0 +1,9 @@
+var tag = new XMLHttpRequest();
+tag.open( "Get", "http://localhost:8080/getNewPost", true, "sa","sa" );
+tag.onreadystatechange() = function recibir(){
+    
+}
+
+
+
+//Tipos de Request 0 - Inicial 1-Open 2-Llega al servidor 3-Cargando Response 4-Done
